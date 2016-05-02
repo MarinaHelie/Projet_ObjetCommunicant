809e97ca88eed5416f041036e9ab9ba15cb1a1b6
lp-phily <tom.phily@live.fr>
2016-05-02 11:27:31 +0200

    Configuration du service WCF

---
+++ ServiceHost/Service1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceObjectsCommunicants;
+
+namespace ServiceHost
+{
+    public partial class Service1 : ServiceBase
+    {
+        internal static System.ServiceModel.ServiceHost myServiceHost = null;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (myServiceHost != null)
+            {
+                myServiceHost.Close();
+            }
+            myServiceHost = new System.ServiceModel.ServiceHost(typeof(ServiceObjectsCommunicants.Service1));
+            myServiceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (myServiceHost != null)
+            {
+                myServiceHost.Close();
+                myServiceHost = null;
+            }
+        }
+    }
+}
