e862f31a9e8a0601970e9974d21173ed152912f1
lp-phily <tom.phily@live.fr>
2016-05-10 11:21:13 +0200

    ajout du serveur nodejs

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var morgan = require('morgan'); // Charge le middleware de logging
+var logger = require('log4js').getLogger('Server');
+var bodyParser = require('body-parser');
+var XMLHttpRequest = require('xhr2');
+var app = express();
+
+var token="iPt5AYfkmCNrvIN1wXzU0Bpw3uXrcfWttfQvALUwqSuser5NDnsPMdzaJr58Wrgn";
+
+// config
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('combined')); // Active le middleware de logging
+
+app.use(express.static(__dirname + '/public')); // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
+
+logger.info('server start');
