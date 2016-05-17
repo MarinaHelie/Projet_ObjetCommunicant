9de7d81310226f9fbe90349eabcc24d81f40dcc5
Régis Parpex <regis.parpex@gmail.com>
2016-05-17 11:34:27 +0200

    Ajout des routes de login/ logout

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan'); // Charge le middleware de logging
 var logger = require('log4js').getLogger('Server');
 var bodyParser = require('body-parser');
+var mysql = require('mysql'); //SI ON UTILISE MYSQL
+var session = require('express-session');
 var XMLHttpRequest = require('xhr2');
 var app = express();
 var Wemo = require('wemo-client');
@@ -37,6 +39,51 @@ app.get('/', function(req, res)
 	  // Turn the switch on
 	  client.setBinaryState(1);
 	});
-}
+});
+
+// LOGIN ---------------------------------------------------------------------------------------------------------------
+app.get('/login', function (req, res) {
+	if (req.session.user) {
+		res.redirect('/equipement');
+	}
+	else {
+		res.render('login');
+	}
+});
+
+
+app.post('/login', function (req, res) {
+	var connection = mysql.createConnection({	//TODO MODIFIER LES INFORMATIONS DE CONNEXION
+		host: 'localhost',
+		user: 'admin',
+		password: '',
+		database: 'HomeMonitoring'
+	});
+	connection.connect();
+	var post = [req.body.login, req.body.password];
+	connection.query("SELECT id, nom, prenom FROM users WHERE email= ? AND password= ?", post, function (err, rows) {
+		if (!err) {
+			req.session.id_user = rows[0]['id'];
+			req.session.login = req.body.login; //EMAIL
+			req.session.nom = rows[0]['nom'];
+			req.session.prenom = rows[0]['prenom'];
+			res.redirect('/equipement');
+		}
+		else {
+			res.redirect('/login');
+		}
+	});
+});
+
+// LOGOUT --------------------------------------------------------------------------------------------------------------
+app.get('/logout', function (req, res) {
+	delete req.session.id_user;
+	delete req.session.login;
+	delete req.session.nom;
+	delete req.session.prenom;
+
+	res.redirect('/login');
+
+});
 
 app.listen(1313); 
